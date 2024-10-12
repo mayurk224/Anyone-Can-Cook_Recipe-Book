05bf56491c126151493df7d8d36f3cc33c69107f
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-12 20:37:31 +0530

    Day 5: made some changes in hero section ui and tags section also in footer

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,35 +2,27 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div>
-      <div className="hero-section bg-cover bg-center h-screen flex flex-col justify-center items-center text-center">
-        {/* Background image or video */}
-        <div className="overlay bg-black bg-opacity-50 absolute inset-0 overflow-hidden">
-          <img
-            src="https://images.pexels.com/photos/1040685/pexels-photo-1040685.jpeg"
-            alt=""
-          />
-        </div>
-
-        {/* Content */}
-        <div className="relative z-10 text-white space-y-4">
-          <h1 className="text-5xl font-bold">
-            Discover Delicious Recipes from Around the World
-          </h1>
-          <p className="text-lg">
-            Explore thousands of easy-to-follow recipes and cook like a pro.
-          </p>
-
-          {/* Search Bar */}
-          
-
-          {/* Call to Action */}
-          <div className="mt-4">
-            <button className="bg-yellow-500 text-white font-semibold px-8 py-3 rounded-full hover:bg-yellow-600 transition">
-              Explore Recipes
-            </button>
-          </div>
-        </div>
+    <div
+      className="rounded-3xl"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <div className="w-full sm:w-3/4 lg:w-1/2 bg-gradient-to-r from-gray-300 to-transparent p-6 sm:p-8 md:p-10 h-full flex flex-col justify-center rounded-3xl space-y-4 min-[425px]:justify-center">
+        <h1 className="text-5xl sm:text-6xl font-semibold mb-3 leading-normal">
+          Discover the secrets of the world's best recipes
+        </h1>
+        <p className="text-lg sm:text-xl leading-relaxed">
+          Experience the magic of cooking with our recipes, tips, and
+          inspiration.
+        </p>
+        <button className="px-8 py-3 rounded-full text-lg font-semibold w-fit text-white bg-black hover:bg-gray-800">
+          Get Started
+        </button>
       </div>
     </div>
   );
